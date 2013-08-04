053ee9c69593ef6ac9427eb0d46f75224d3c7bfa
Gerald Thomas <gerald-thomas@github.com>
2013-08-04 18:52:46 +1000

    DRY tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,38 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:page_title)  { '' }
+    let (:heading)    { 'Sample App' }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:page_title)  { 'Help' }
+    let(:heading)  { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:page_title)  { 'About' }
+    let(:heading)  { 'About' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:page_title)  { 'Contact' }
+    let(:heading)  { 'Contact' }
+    it_should_behave_like "all static pages"
   end
 end
